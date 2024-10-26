b871e7241ff06b315d815fca5d34a4c6f42261c9
nonisomorphiclinearmap <allenli0225@gmail.com>
2024-10-26 16:10:59 -0400

    added appbuilder

---
+++ src/main/java/app/AppBuilder.java
@@ -0,0 +1,12 @@
+package app;
+
+import javax.swing.*;
+
+/**
+ * The AppBuilder class is responsible for putting together the pieces of
+ * our CA architecture; piece by piece.
+ * <p/>
+ * This is done by adding each View and then adding related Use Cases.
+ */
+public class AppBuilder {
+}
