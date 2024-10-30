0c45b5b98ba2eecd0d11ee2dc5c9350d95e08daf
serene <serenestoller@gmail.com>
2024-10-29 23:28:54 -0400

    Implement ImageDataBase

--- src/main/java/data_access/ImageDataBase.java
+++ src/main/java/data_access/ImageDataBase.java
@@ -1,4 +1,29 @@
 package data_access;
 
+import java.awt.image.BufferedImage;
+
+/**
+ * ImageDB is an interface that defines the methods that the ImageDB class must implement.
+ */
 public interface ImageDataBase {
+    /**
+     * A method that returns the image with the given id.
+     * @param id is the unique id of the desired image
+     * @return the image with the given id
+     */
+     BufferedImage getImageFromID(String id);
+
+    /**
+     * A method that inserts a new image into the database.
+     * @param image is the image to insert
+     * @return the id of the inserted image
+     */
+    String addImage(BufferedImage image);
+
+    /**
+     * A method that deletes the image with the given id.
+     * @param id is the id of the image to delete
+     * @return whether the image was successfully deleted
+     */
+    boolean deleteImage(String id);
 }
