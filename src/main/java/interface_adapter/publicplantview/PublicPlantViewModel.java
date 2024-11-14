6e0b21ec709e8298a5fcdc2abf6da80174542327
phytorchumsworth <AliKarimLalani@github.com>
2024-11-14 01:04:07 -0500

    Just added the classes to be worked with for the PublicPlantView for now (Pretty rushed, apologies !)
    Will work on it more today and add an additional Owner text field to see who owns the plant later today.

---
+++ src/main/java/interface_adapter/publicplantview/PublicPlantViewModel.java
@@ -0,0 +1,10 @@
+package interface_adapter.publicplantview;
+
+import interface_adapter.ViewModel;
+
+
+public class PublicPlantViewModel {
+
+    public static final String RETURN_BUTTON_LABEL = "← Return";
+
+}
