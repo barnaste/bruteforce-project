59572ef595c48c28163ec6ba7a5bd2494cd324db
Areez <areez@chishtie.com>
2024-11-25 17:25:55 -0500

    Implement unit tests for entities

--- src/test/java/entity/PlantTest.java
+++ src/test/java/entity/PlantTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 public class PlantTest {
     @Test
-    public void successTest() {
-
+    public void setCommentsTest() {
+        Plant plant = new Plant();
+        plant.setComments("My comments");
+        assertEquals("My comments", plant.getComments());
     }
 }
--- src/test/java/entity/UserTest.java
+++ src/test/java/entity/UserTest.java
@@ -1,4 +1,14 @@
 package entity;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
+    @Test
+    public void setCommentsTest() {
+        User user = new User();
+        user.setUsername("arz");
+        assertEquals("arz", user.getUsername());
+    }
 }
